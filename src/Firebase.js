25c8554f6d73a37d53f67121731f6e070c28025d
Bilal Fayyaz <33794813+bilalfiaz5@users.noreply.github.com>
2020-09-18 16:58:00 +0500

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,14 +3,7 @@
 
 
   const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDeIXZxoDojdcJssztmQXKlxcGt4soy9Gw",
-    authDomain: "todo-app-3fbd8.firebaseapp.com",
-    databaseURL: "https://todo-app-3fbd8.firebaseio.com",
-    projectId: "todo-app-3fbd8",
-    storageBucket: "todo-app-3fbd8.appspot.com",
-    messagingSenderId: "890795014572",
-    appId: "1:890795014572:web:e030eaaa447d7f5f9fa209",
-    measurementId: "G-MSK9H5YW30"
+   //firebaseConfig will come here. copy it from firebase console and paste the code inside the curly brackets here.
   });
   const db = firebaseApp.firestore();
   
